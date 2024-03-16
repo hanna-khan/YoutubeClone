9317c3c042cd416d92004c469da537581c84bfe6
Hania Imran <hanna.khan205@gmail.com>
2024-03-17 01:10:33 +0500

    Initial commit

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// with promises
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+export { asyncHandler };
+
+// with try catch
+// const asyncHandler = (func) => async () => {
+//   try {
+//     await func(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
